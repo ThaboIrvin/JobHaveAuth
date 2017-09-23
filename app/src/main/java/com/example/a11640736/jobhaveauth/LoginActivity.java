969ec95c415dc09d7278b6c1a4549cde35ca6b54
ThaboIrvin <Mateta.t.i@gmail.com>
2017-09-23 16:19:07 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/a11640736/jobhaveauth/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.example.a11640736.jobhaveauth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
